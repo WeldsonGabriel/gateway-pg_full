1155c9c924238d3f1d3cb5ff53d5dc1039d33520
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-04-11 00:37:19 -0300

    "invoce finalizado"

--- internal/service/invoice_service.go
+++ internal/service/invoice_service.go
@@ -17,7 +17,7 @@ func NewInvoiceService(invoiceRepository domain.InvoiceRepository, accountServic
 	}
 }
 
-func (s *InvoiceService) Create(input *dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
+func (s *InvoiceService) Create(input dto.CreateInvoiceInput) (*dto.InvoiceOutput, error) {
 	account, err := s.accountService.FindByAPIKey(input.APIKey)
 	if err != nil {
 		return nil, err
--- internal/web/server/server.go
+++ internal/web/server/server.go
@@ -2,37 +2,49 @@ package server
 
 import (
 	"net/http"
-	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
-	"github.com/go-chi/chi/v5"
-	"github.com/devfullcycle/imersao22/go-gateway/internal/web/handlers"
 
+	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/web/handlers"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/web/middleware"
+	"github.com/go-chi/chi/v5"
 )
+
 type Server struct {
-	router *chi.Mux
-	server *http.Server
+	router         *chi.Mux
+	server         *http.Server
 	accountService *service.AccountService
-	port string
+	invoiceService *service.InvoiceService
+	port           string
 }
 
-func NewServer(port string, accountService *service.AccountService) *Server {
+func NewServer(port string, accountService *service.AccountService, invoiceService *service.InvoiceService) *Server {
 	return &Server{
-		router: chi.NewRouter(),
+		router:         chi.NewRouter(),
 		accountService: accountService,
-		port: port,
+		invoiceService: invoiceService,
+		port:           port,
 	}
-}	
- func (s *Server) ConfigureRoutes() {
+}
+func (s *Server) ConfigureRoutes() {
 	accountHandler := handlers.NewAccountHandler(s.accountService)
+	invoiceHandler := handlers.NewInvoiceHandler(s.invoiceService)
+	authMiddleware := middleware.NewAuthMiddleware(s.accountService)
+
 	s.router.Post("/accounts", accountHandler.Create)
 	s.router.Get("/accounts", accountHandler.Get)
- }
- func (s *Server) Start() error {
+
+	s.router.Group(func(r chi.Router) {
+		r.Use(authMiddleware.Middleware)
+		r.Post("/invoice", invoiceHandler.Create)
+		r.Get("/invoice", invoiceHandler.ListByAccount)
+		r.Get("/invoice/{id}", invoiceHandler.Get)
+	})
+}
+
+func (s *Server) Start() error {
 	s.server = &http.Server{
-		Addr: ":" + s.port,
+		Addr:    ":" + s.port,
 		Handler: s.router,
 	}
 	return s.server.ListenAndServe()
- }
-
-
-
+}
