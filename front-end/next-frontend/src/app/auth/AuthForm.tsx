c4abd6ea3dd1df308e9af84998cedac02b27d768
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-04-13 14:07:01 -0300

    "atualizando http"

--- front-end/next-frontend/src/app/auth/AuthForm.tsx
+++ front-end/next-frontend/src/app/auth/AuthForm.tsx
@@ -9,7 +9,7 @@ export async function loginAction(formData: FormData) {
     "use server";
     const apiKey = formData.get("apiKey");
 
-    const response = await fetch("https://localhost:8080/accounts", {
+    const response = await fetch("http://localhost:8080/accounts", {
         headers: {
             'X-API-Key': apiKey as string,
 
